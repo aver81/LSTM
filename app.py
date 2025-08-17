0470b5c1cdbe40d05231bb29ae5cc68139f0a4d3
Aayush Verma <44445921+aver81@users.noreply.github.com>
2025-08-17 18:42:12 -0500

    Update app.py
    Adding LSTM and GRU capabilities

--- app.py
+++ app.py
@@ -7,9 +7,9 @@ from tensorflow.keras.preprocessing.sequence import pad_sequences
 
 model_option = st.selectbox("Choose a model:", ["LSTM", "GRU"])
 
-if model_option == "Model v1":
+if model_option == "LSTM":
     model = load_model("model.h5")
-elif model_option == "Model v2":
+elif model_option == "GRU":
     model = load_model("GRU model.h5")
 
 with open('tokenizer.pkl','rb') as file:
@@ -42,4 +42,5 @@ n_words = int(st.number_input('Enter how many words you want to predict: ',3))
 if st.button('Predict'):
     predicted_words = next_n_word_predictions(text,n_words)
     st.text_area(f'📘 Predicted text (next {n_words} words):', predicted_words)
+
 
