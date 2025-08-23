1651d09c824cdfb3237b816ca9e3e07ec28dff46
Aayush Verma <44445921+aver81@users.noreply.github.com>
2025-08-23 13:07:14 -0500

    Minor Aesthetic changes

--- app.py
+++ app.py
@@ -5,15 +5,6 @@ import pickle
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 
-model_option = st.selectbox("Choose a model:", ["LSTM", "GRU"])
-
-if model_option == "LSTM":
-    model = load_model("model.h5")
-elif model_option == "GRU":
-    model = load_model("GRU model.h5")
-
-with open('tokenizer.pkl','rb') as file:
-    tokenizer=pickle.load(file)
 
 max_sequence_len = model.input_shape[1] + 1
 
@@ -35,12 +26,22 @@ def next_n_word_predictions(text,n_words):
 
 
 st.title('LSTM N-Word Prediction with Dropout')
+model_option = st.selectbox("Choose a model:", ["LSTM", "GRU"])
+
+if model_option == "LSTM":
+    model = load_model("model.h5")
+elif model_option == "GRU":
+    model = load_model("GRU model.h5")
+
+with open('tokenizer.pkl','rb') as file:
+    tokenizer=pickle.load(file)
 
 text = st.text_input('Enter the sentence:',"to be or not to be")
 n_words = int(st.number_input('Enter how many words you want to predict: ',3))
 
 if st.button('Predict'):
     predicted_words = next_n_word_predictions(text,n_words)
-    st.text_area(f'📘 Predicted text (next {n_words} words):', predicted_words)
+    st.text_area(f'Predicted text (next {n_words} words):', predicted_words)
+
 
 
